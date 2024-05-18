02040720955d621897bbdc17036f69adabf1601e
Abed <ngangaabed@gmail.com>
2024-05-18 13:52:37 +0300

    Specific recipe view and path

--- recipes/urls.py
+++ recipes/urls.py
@@ -21,4 +21,5 @@ from recipeKeeper import views
 urlpatterns = [
     path("", views.index, name="index"),
     path('admin/', admin.site.urls),
+    path("<int:recipe_id>/", views.get_recipe_by_id, name="get_recipe_by_id"),
 ]
