5d6ed4cd51f683981b47ebf199ba84e792dad1f7
Abed <ngangaabed@gmail.com>
2024-05-18 14:48:03 +0300

    Added button navigations

--- recipeKeeper/views.py
+++ recipeKeeper/views.py
@@ -5,6 +5,7 @@ from.models import Recipe
 from django.template import loader
 from .forms import RecipeForm
 from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
 
 
 def index(request):
@@ -16,14 +17,24 @@ def get_recipe_by_id(request, recipe_id):
     template = loader.get_template("recipeKeeper/recipe_template.html")
     return HttpResponse(template.render(context, request))
 
+@login_required
 def add_recipe(request):
     if request.method == 'POST':
         form = RecipeForm(request.POST)
+        
         if form.is_valid():
-            form.save()
-            return redirect('recipe_list')
+            recipe = form.save(commit=False)
+            recipe.author = request.user
+            recipe.save()
+
+            # Get the ID of the newly added recipe
+            new_recipe_id = recipe.pk
+            return redirect('get_recipe_by_id', recipe_id=new_recipe_id)
     else:
         form = RecipeForm()
     context = {'form': form}
     template = loader.get_template("recipeKeeper/add_recipe.html")
     return HttpResponse(template.render(context, request))
+
+def recipe_list(request):
+    pass
