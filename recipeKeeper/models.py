bd2b210285f2e0c0b2160a3e066adcc1a478d4e5
Abed <ngangaabed@gmail.com>
2024-05-18 12:02:16 +0300

    M:1 recipe:user

--- recipeKeeper/models.py
+++ recipeKeeper/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Recipe(models.Model):
     title = models.CharField(max_length=200)
@@ -11,6 +12,8 @@ class Recipe(models.Model):
         ('Hard', 'Hard'),
     ]
     difficulty_level = models.CharField(max_length=6, choices=difficulty)
-    
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
     def __str__(self):
         return self.title
