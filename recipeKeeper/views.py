6badf4cf55776071acf6c64c86ced6645b960578
Abed <ngangaabed@gmail.com>
2024-05-18 13:51:13 +0300

    Added template urls to settings

--- recipeKeeper/views.py
+++ recipeKeeper/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+from.models import Recipe
+from django.template import loader
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Hello, world. You're at the Recipes Homepage.")
+
+def get_recipe_by_id(request, recipe_id):
+    # Retrieve the recipe object by its ID
+    recipe = get_object_or_404(Recipe, pk=recipe_id)
+    # Pass the recipe object to the template context
+    context = {'recipe': recipe}
+    template = loader.get_template("recipeKeeper/recipe_template.html")
+    # Render the template with the recipe details
+    return HttpResponse(template.render(context, request))
