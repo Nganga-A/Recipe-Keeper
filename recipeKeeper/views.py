1ee410f586664728067f26686b74c7f98382648d
Abed <ngangaabed@gmail.com>
2024-05-18 14:27:28 +0300

    Add recipe view

--- recipeKeeper/views.py
+++ recipeKeeper/views.py
@@ -3,16 +3,27 @@ from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from.models import Recipe
 from django.template import loader
+from .forms import RecipeForm
+from django.shortcuts import redirect
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the Recipes Homepage.")
 
 def get_recipe_by_id(request, recipe_id):
-    # Retrieve the recipe object by its ID
     recipe = get_object_or_404(Recipe, pk=recipe_id)
-    # Pass the recipe object to the template context
     context = {'recipe': recipe}
     template = loader.get_template("recipeKeeper/recipe_template.html")
-    # Render the template with the recipe details
+    return HttpResponse(template.render(context, request))
+
+def add_recipe(request):
+    if request.method == 'POST':
+        form = RecipeForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('recipe_list')
+    else:
+        form = RecipeForm()
+    context = {'form': form}
+    template = loader.get_template("recipeKeeper/add_recipe.html")
     return HttpResponse(template.render(context, request))
