d0170b4ad5fffd107a4f5e5673892ac1360788df
Abed <ngangaabed@gmail.com>
2024-05-18 11:20:59 +0300

    Models Setup

--- recipeKeeper/models.py
+++ recipeKeeper/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Recipe(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    ingredients = models.TextField()
+    cooking_instructions = models.TextField()
+    difficulty = [
+        ('Easy', 'Easy'),
+        ('Medium', 'Medium'),
+        ('Hard', 'Hard'),
+    ]
+    difficulty_level = models.CharField(max_length=6, choices=difficulty)
+    
+    def __str__(self):
+        return self.title
